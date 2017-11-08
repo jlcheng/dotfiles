5eebc59606e1c5e0d1ddba2f754387264ae2c7d6
John Cheng <johnlicheng@gmail.com>
2017-11-08 09:19:43 -0800

    Add configuration for work (OS X) system

--- bashrc_jc.sh
+++ bashrc_jc.sh
@@ -2,20 +2,14 @@
 #  echo ". ~/github/dotfiles/bashrc_jc.sh" >> ~/.bashrc
 
 export HISTCONTROL=ignoredups
+export HISTFILE=$HOME/.bash_history
 export HISTFILESIZE=500
 export HISTIGNORE='&:ls:[bf]g:exit'
 export IFS=$' \t\n'
-export PATH='/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:'
+export PATH='/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/bin:/bin:/usr/sbin:/sbin'
 export PS1='\w$ '
-alias emacs="/usr/bin/emacsclient -n"
+alias emacs="emacsclient -n"
 export EDITOR=vi
-
-# export PIP_REQUIRE_VIRTUALENV=true
-# export WORKON_HOME=$HOME/.virtualenvs
-# export PROJECT_HOME=$HOME/Devel
-# source /usr/local/share/python/virtualenvwrapper.sh
-# export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
-# export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 f ()
 {
---
+++ bashrc_jc_osx.sh
@@ -0,0 +1,13 @@
+# Installation ---
+#  echo ". ~/github/dotfiles/bashrc_jc_osx.sh" >> ~/.bashrc
+
+export PATH='/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/Users/jcheng/.rvm/bin:/Users/jcheng/software/play-2:/Users/jcheng/bin:/Applications/Sublime Text 2.app/Contents/SharedSupport/bin'
+
+# https://emacsformacosx.com/
+if [ -d "/Applications/Emacs.app" ]; then 
+  alias emacs="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
+fi
+
+export JAVA_HOME=`ls -d /Library/Java/JavaVirtualMachines/* | tail -n 1`
+
+echo "bashrc_jc_osx.sh"
