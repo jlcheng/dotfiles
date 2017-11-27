8eeabd37d057c5a0fe2cd8475a56a53f593e5f99
John Cheng <johnlicheng@gmail.com>
2017-11-27 11:14:13 -0800

    refactor code to start ssh-agent

--- bashrc_jc.sh
+++ bashrc_jc.sh
@@ -22,4 +22,10 @@ fe ()
     find . -name "*.${1}"
 }
 
+SSHAGENT="ssh-agent"
+if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
+    # use eval to take the output of sshagent and set SSH_AUTH_SOCK and SSH_AGENT_ID
+    eval `$SSHAGENT`
+fi
+
 echo "bashrc_jc.sh"
--- bashrc_jc_cygwin.sh
+++ bashrc_jc_cygwin.sh
@@ -1,10 +1,6 @@
 # Installation ---
 #  echo ". ~/github/dotfiles/bashrc_jc_cygwin.sh" >> ~/.bashrc
 
-SSHAGENT="ssh-agent"
-if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
-    # use eval to take the output of sshagent and set SSH_AUTH_SOCK and SSH_AGENT_ID
-    eval `$SSHAGENT`
-    # kill ssh-agent on exit
-    trap "kill $SSH_AGENT_PID" 0
-fi
+# placeholder
+
+echo "bashrc_jc_cygwin.sh"
