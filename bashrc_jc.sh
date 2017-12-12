ab248b73ce2adab08c1763611050976b40da60fc
John Cheng <johnlicheng@gmail.com>
2017-12-12 14:55:02 -0800

    Add functions to manage tab titles

--- bashrc_jc.sh
+++ bashrc_jc.sh
@@ -22,6 +22,29 @@ fe ()
     find . -name "*.${1}"
 }
 
+# title_management
+# jc_tab_max: controls the max length of a tab title
+# jc_tab_title <name>: appends a custom label to the tab
+export jc_tab_max=20
+PROMPT_COMMAND='
+if [ -n "$jc_tab_title" ]; then
+  title__="$PWD ($jc_tab_title)"
+else
+  title__=$PWD
+fi
+if [ ${#title__} -gt $jc_tab_max ]; then
+  title__="...`echo $title__ | tail -c $jc_tab_max`"
+fi
+echo -n -e "\033]0;$title__\007"
+'
+jc_tab_title()
+{
+  if [ -n "$1" ]; then
+     export jc_tab_title=$1
+  fi
+}
+# /title management
+
 SSHAGENT="ssh-agent"
 if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
     # use eval to take the output of sshagent and set SSH_AUTH_SOCK and SSH_AGENT_ID
