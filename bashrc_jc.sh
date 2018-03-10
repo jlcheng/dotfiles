acb01f42c3fa9d0af09150a4ee25785724c714db
John Cheng <johnlicheng@gmail.com>
2018-03-09 20:11:30 -0800

    Reuse ssh-agent across sessions.

--- bashrc_jc.sh
+++ bashrc_jc.sh
@@ -64,15 +64,21 @@ jfe ()
 }
 
 # ssh
-alias ssh="ssh -q"
-jsa ()
-{
-    ssh-add -qL &> /dev/null
-    if [ "$?" == "2" ]; then
-        eval `ssh-agent`
-        ssh-add ~/.ssh/jackrabbit_rsa
+SSH_TMP=~/.ssh-agent.tmp
+if [ -f $SSH_TMP ]; then
+    SSH_PID=`cat ~/.ssh-agent.tmp | sed  -rn 's/.+pid ([0-9]+);/\1/p'`
+    if [ "$?" == "0" ]; then
+        ps -p $SSH_PID > /dev/null
+        if [ "$?" == "0" ]; then
+	    . $SSH_TMP
+        fi
     fi
-}
+fi
+if [ -z "$SSH_AGENT_PID" ]; then
+    ssh-agent > $SSH_TMP
+    . $SSH_TMP
+fi
+alias ssh="ssh -q"
 # /ssh
 
 # title_management
