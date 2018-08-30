eefd2a0d2acfdb1ce5863010fc4e41bdf51648d8
John Cheng <johnlicheng@gmail.com>
2018-08-30 09:17:46 -0700

    bash: Add /usr/local/go/bin for osx

--- bashrc_jc_osx.sh
+++ bashrc_jc_osx.sh
@@ -1,7 +1,9 @@
 # Installation ---
 #  echo ". ~/privprjs/dotfiles/bashrc_jc_osx.sh" >> ~/.bashrc
 
-export PATH='/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/Users/jcheng/.rvm/bin:/Users/jcheng/software/play-2:/Users/jcheng/bin:/Applications/Sublime Text 2.app/Contents/SharedSupport/bin'
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/Users/jcheng/.rvm/bin:/Users/jcheng/software/play-2:/Users/jcheng/bin:/Applications/Sublime Text 2.app/Contents/SharedSupport/bin"
+export PATH="$PATH:/usr/local/go/bin"
+
 
 # https://emacsformacosx.com/
 if [ -d "/Applications/Emacs.app" ]; then 
