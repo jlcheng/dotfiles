7b4f23e54a87b7dd652918c4236a4cf72846d7f6
John Cheng <johnlicheng@gmail.com>
2019-01-02 07:57:18 -0800

    Revert "grs-autocommit:2018-01-02T07:41:01-08:00"
    This reverts commit 8726f67ec365ab1cdaa63724fe524c7d2812c2ff.

--- site-start.el
+++ site-start.el
@@ -92,9 +92,9 @@
 (global-set-key (kbd "M-n M-b") 'jsnice-jc)
 (global-set-key (kbd "M-n M-s") 'whitespace-mode)
 
-;;; Only start a server when it is not running
+;;; enable emacsclient support unless we're running 'emacs-nox'
 ; note: string-match-p not avail on Emacs 22.1.1 on MacOS (latest release is 25.3 as of Sept 2017)
-(unless (server-running-p)
+(unless (server-running-p)    
   ;; runs emacs server
   (server-start)
   ;; Ubuntu: run a no-op command to bring the window into focus
