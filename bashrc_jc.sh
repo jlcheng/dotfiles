14edd6f50f5dc79a3dc82a9e3cfd24bd8a3f9192
John Cheng <johnlicheng@gmail.com>
2019-02-17 10:29:48 -0800

    ssh: auto init ssh-agent

--- bashrc_jc.sh
+++ bashrc_jc.sh
@@ -89,7 +89,19 @@ if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
     export PATH="$PATH:$HOME/bin"
 fi
 
+# set environment variables if user's agent already exists
+[ -z "$SSH_AUTH_SOCK" ] && SSH_AUTH_SOCK=$(ls -l /tmp/ssh-*/agent.* 2> /dev/null | grep $(whoami) | awk '{print $9}')
+[ -z "$SSH_AGENT_PID" -a -z `echo $SSH_AUTH_SOCK | cut -d. -f2` ] && SSH_AGENT_PID=$((`echo $SSH_AUTH_SOCK | cut -d. -f2` + 1))
+[ -n "$SSH_AUTH_SOCK" ] && export SSH_AUTH_SOCK
+[ -n "$SSH_AGENT_PID" ] && export SSH_AGENT_PID
+
+# start agent if necessary
+if [ -z $SSH_AGENT_PID ] && [ -z $SSH_TTY ]; then  # if no agent & not in ssh
+  eval `ssh-agent -s` > /dev/null
+fi
+
 sjc () {
+    
     if [ -f ~/.ssh/autokey ]; then
 	ssh-add ~/.ssh/autokey
     fi
