130f947b83a346eec2f21f64a295c5b105b80c51
John Cheng <johnlicheng@gmail.com>
2019-05-30 07:15:41 -0700

    grs-autocommit:2019-05-30T07:15:41-07:00

--- bashrc_jc_linux.sh
+++ bashrc_jc_linux.sh
@@ -1,20 +1,25 @@
 # === START: ssh ===
-if [ -z "SSH_AUTH_SOCK" ]; then
-    SSH_TMP=~/.ssh-agent.tmp
-    if [ -f $SSH_TMP ]; then
-	SSH_PID=`cat ~/.ssh-agent.tmp | sed  -rn 's/.+pid ([0-9]+);/\1/p'`
-	if [ "$?" == "0" ] && [ -n "$SSH_PID" ]; then
-	    ps -p $SSH_PID > /dev/null
-	    if [ "$?" == "0" ]; then
-		. $SSH_TMP
+jc.ssh.sock() {
+    if [ -z "SSH_AUTH_SOCK" ]; then
+	# If $SSH_AUTH_SOCK is not set, try to find it
+	SSH_TMP=~/.ssh-agent.tmp
+	if [ -f $SSH_TMP ]; then
+	    SSH_PID=`cat ~/.ssh-agent.tmp | sed  -rn 's/.+pid ([0-9]+);/\1/p'`
+	    if [ "$?" == "0" ] && [ -n "$SSH_PID" ]; then
+		ps -p $SSH_PID > /dev/null
+		if [ "$?" == "0" ]; then
+		    . $SSH_TMP
+		fi
 	    fi
 	fi
+	if [ -z "$SSH_AGENT_PID" ]; then
+	    ssh-agent > $SSH_TMP
+	    . $SSH_TMP
+	fi
+    else
+	# Check that 
     fi
-    if [ -z "$SSH_AGENT_PID" ]; then
-	ssh-agent > $SSH_TMP
-	. $SSH_TMP
-    fi
-fi
+}
 # === END: ssh ===
 
 # === START: aws ===
