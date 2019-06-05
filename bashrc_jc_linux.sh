722b90ae8b27728c46508ad56bc4652d1bc84dd5
John Cheng <johnlicheng@gmail.com>
2019-06-05 15:14:20 -0700

    grs-autocommit:2019-06-05T15:14:20-07:00

--- bashrc_jc_linux.sh
+++ bashrc_jc_linux.sh
@@ -1,19 +1,10 @@
 # === START: ssh ===
-jc.ssh.sock() {
-    socket_file=~/.ssh/agent_socket_tmp    
-    # If $SSH_AUTH_SOCK is not set, try to find it within $socket_file
-    if [[ -z "SSH_AUTH_SOCK" ]]; then
-	if [[ -f $socket_file ]]; then
-	    . $socket_file
-	fi
-    fi
-
-    do_agent=0
-    # Now we have a $SSH_AUTH_SOCK that is possibly invalid
-    if [[ ! -S $SSH_AUTH_SOCK ]] ; then
-
-    fi
-}
+# If $SSH_AUTH_SOCK is not set, create a new agent
+if [[ -z $SSH_AUTH_SOCK ]] || ; then
+    eval `ssh-agent -s`
+    trap 'test -n "$SSH_AGENT_PID" && eval `/usr/bin/ssh-agent -k`' 0
+    return
+fi
 # === END: ssh ===
 
 # === START: aws ===
