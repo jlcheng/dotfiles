c930e577bc386afa366f9bfa344b812b188a296e
John Cheng <johnlicheng@gmail.com>
2019-06-27 17:31:10 -0700

    grs-autocommit:2019-06-27T17:31:10-07:00

--- bashrc_jc_linux.sh
+++ bashrc_jc_linux.sh
@@ -1,6 +1,6 @@
 # === START: ssh ===
 # If $SSH_AUTH_SOCK is not set, create a new agent
-if [[ -z $SSH_AUTH_SOCK ]] || ; then
+if [[ -z $SSH_AUTH_SOCK ]] ; then
     eval `ssh-agent -s`
     trap 'test -n "$SSH_AGENT_PID" && eval `/usr/bin/ssh-agent -k`' 0
     return
