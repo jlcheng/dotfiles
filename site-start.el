1a5217e5353aa0e06bec9e6e55453d2fe20447b3
John Cheng <johnlicheng@gmail.com>
2019-09-13 08:59:01 -0700

    grs-autocommit:2019-09-13T08:59:01-07:00

--- site-start.el
+++ site-start.el
@@ -253,8 +253,10 @@
 
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin")) ;; Needed for M-x shell-command
 (setenv "PATH" (concat (getenv "PATH") (concat ":" (expand-file-name "~/go/bin"))))
+(setenv "PATH" (concat (getenv "PATH") ":/Library/Frameworks/Python.framework/Versions/3.6/bin/mypy")) ;; hack for mac os
 (add-to-list 'exec-path "/usr/local/bin")                  ;; Needed for (executable-find ...)
 (add-to-list 'exec-path (expand-file-name "~/go/bin"))
+(add-to-list 'exec-path "/Library/Frameworks/Python.framework/Versions/3.6/bin/mypy") ;; hack for mac os
 (if (functionp 'global-company-mode) (global-company-mode))
 (if (functionp 'which-key-mode) (which-key-mode))
 
