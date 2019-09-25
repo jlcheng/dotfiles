a695f4e7bb3f3735be2023a4535fff8e2e2f5571
John Cheng <johnlicheng@gmail.com>
2019-09-25 16:42:08 +0000

    installes emacsy files

--- install.py
+++ install.py
@@ -5,13 +5,18 @@ import os.path
 def install_line(cid, file_name, line):
     file_name = os.path.expanduser(file_name)
     do_install = False
-    with open(file_name) as f:
+
+    if not os.path.exists(file_name):
+        with open(file_name, "a+"):
+            pass
+    
+    with open(file_name, "r") as f:
         if line not in f.read():
             do_install = True
     
     if do_install:
         print(f"installing {cid} to {file_name}")
-        with open(file_name, "a") as f:
+        with open(file_name, "a+") as f:
             f.write(line)
             f.write("\n")
     else:
@@ -19,6 +24,7 @@ def install_line(cid, file_name, line):
 
 def main():
     install_line("bashrc_jc.sh", "~/.bashrc", ". ~/privprjs/dotfiles/bashrc_jc.sh")
+    install_line("init.el", "~/.emacs.d/init.el", '(load-file (expand-file-name "~/privprjs/dotfiles/site-start.el"))')
 
 
 if __name__ == '__main__':
