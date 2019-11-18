3b30c9a5223c0f3ceadfe8908058e96d37c0f60f
John Cheng <johnlicheng@gmail.com>
2019-11-18 07:51:00 -0800

    grs-autocommit:2019-11-18T07:51:00-08:00

--- install.py
+++ install.py
@@ -3,6 +3,7 @@
 import os.path
 import os
 
+
 def install_line(cid, file_name, line):
     file_name = os.path.expanduser(file_name)
     do_install = False
@@ -10,11 +11,11 @@ def install_line(cid, file_name, line):
     if not os.path.exists(file_name):
         with open(file_name, "a+"):
             pass
-    
+
     with open(file_name, "r") as f:
         if line not in f.read():
             do_install = True
-    
+
     if do_install:
         print(f"installing {cid} to {file_name}")
         with open(file_name, "a+") as f:
@@ -22,6 +23,7 @@ def install_line(cid, file_name, line):
             f.write("\n")
     else:
         print(f"{cid} already installed")
+
 
 def install_link(src, dst):
     dst = os.path.expanduser(dst)
@@ -32,14 +34,15 @@ def install_link(src, dst):
         os.symlink(os.path.abspath(src), dst)
     else:
         print(f"{src} already installed")
-    
-        
+
+
 def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
+
     install_line("bashrc_jc.sh", "~/.bashrc", ". ~/privprjs/dotfiles/bashrc_jc.sh")
     install_line("init.el", "~/.emacs.d/init.el", '(load-file (expand-file-name "~/privprjs/dotfiles/site-start.el"))')
     install_link("tmux.conf", "~/.tmux.conf")
+    install_link("flake8rc", "~/.flake8rc")
 
 
 if __name__ == '__main__':
