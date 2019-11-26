ec863b204de15fbe0fd350eab0c0b5940dd3c46c
John Cheng <johnlicheng@gmail.com>
2019-11-26 07:29:36 -0800

    grs-autocommit:2019-11-26T07:29:36-08:00

--- site-start.el
+++ site-start.el
@@ -183,7 +183,6 @@
     (define-key jc/left-map (kbd "C-d") 'godoc))
   (add-hook 'go-mode-hook 'jc/go-mode-hook)
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-hook 'go-mode-hook 'lsp-deferred)
   )
 (jc/go-mode-init)
 ;; === STOP: go-mode ===
