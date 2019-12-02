92caf0740bd7b8cade581c06e0f88f2e716f3cd6
John Cheng <johnlicheng@gmail.com>
2019-12-02 07:24:29 -0800

    grs-autocommit:2019-12-02T07:24:29-08:00

--- site-start.el
+++ site-start.el
@@ -152,7 +152,7 @@
 (custom-set-variables
  '(org-todo-keywords '((sequence "TODO" "|" "DONE")
 		       (sequence "REPORT" "BUG" "KNOWNCAUSE" "|" "FIXED") ;; 2019-11-25 - example only. 
-		       (sequence "WK" "WK_CODING" "WK_PENDING_PR" "|" "WK_MERGED")
+		       (sequence "WK_CODING" "WK_PENDING_PR" "|" "WK_MERGED")
 		       (sequence "WK_TODO" "WK_DOING" "|" "WK_DONE")
 		       (sequence "UNPAUSED" "PUNTED" "|" "PAUSED")
         (sequence "|" "CANCELED")))
