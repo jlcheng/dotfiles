e41b1d822440ec9847344434c295162a14e70816
John Cheng <johnlicheng@gmail.com>
2020-03-03 17:57:15 -0800

    grs-autocommit:2020-03-03T17:57:15-08:00

--- site-start.el
+++ site-start.el
@@ -170,7 +170,7 @@
  ; themes
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (manoj-dark))) 
+ '(custom-enabled-themes (quote (deeper-blue))) 
  )
 
 
