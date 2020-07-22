cb1a652ddff9e2e29249b5ce41da2c7db1491f04
John Cheng <johnlicheng@gmail.com>
2020-07-22 08:13:32 -0700

    grs-autocommit:2020-07-22T08:13:32-07:00

--- bashrc_jc_linux.sh
+++ bashrc_jc_linux.sh
@@ -7,6 +7,12 @@ if [[ -z $SSH_AUTH_SOCK ]] ; then
 fi
 # === END: ssh ===
 
+# === START: kde ===
+if [[ -f /usr/bin/kde-open5 ]]; then
+    alias open=/usr/bin/kde-open5
+fi
+# === END: kde ===
+
 # === START: terraform ===
 # Setting TF_DATA_DIR is a bad idea. It forces all terraform workspaces to use the same terraform version
 # in terraform.required_version file
