ee265f37eecaa397181e819fd13b59b7e6ea03df
John Cheng <johnlicheng@gmail.com>
2021-01-10 08:17:28 -0800

    grs-autocommit:2021-01-10T08:17:28-08:00

--- install.py
+++ install.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import os.path
 import os
 
 
 def install_line(cid, file_name, line):
-    file_name = os.path.expanduser(file_name)
+    file_path = Path(file_name).expanduser()
     do_install = False
 
-    if not os.path.exists(file_name):
-        with open(file_name, "a+"):
-            pass
+    file_path.parent.mkdir(mode=0o777, parents=True, exist_ok=True)
+    file_path.touch(mode=0o666, exist_ok=True)
 
-    with open(file_name, "r") as f:
+    with file_path.open(mode="r") as f:
         if line not in f.read():
             do_install = True
 
     if do_install:
         print(f"installing {cid} to {file_name}")
-        with open(file_name, "a+") as f:
+        with file_path.open("a+") as f:
             f.write(line)
             f.write("\n")
     else:
@@ -26,10 +26,9 @@ def install_line(cid, file_name, line):
 
 
 def install_link(src, dst):
-    dst = os.path.expanduser(dst)
-    do_install = not os.path.exists(dst)
+    dst = Path(dst).expanduser()
 
-    if do_install:
+    if not dst.exists():
         print(f"installing {src} to {dst}")
         os.symlink(os.path.abspath(src), dst)
     else:
@@ -40,10 +39,14 @@ def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     install_line("bashrc_jc.sh", "~/.bashrc", ". ~/privprjs/dotfiles/bashrc_jc.sh")
-    install_line("init.el", "~/.emacs.d/init.el", '(load-file (expand-file-name "~/privprjs/dotfiles/site-start.el"))')
+    install_line(
+        "init.el",
+        "~/.emacs.d/init.el",
+        '(load-file (expand-file-name "~/privprjs/dotfiles/site-start.el"))',
+    )
     install_link("tmux.conf", "~/.tmux.conf")
     install_link("flake8rc", "~/.flake8rc")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
