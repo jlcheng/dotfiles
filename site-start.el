729f25a08ba08b7552b691cdee9e5a3898edbbb9
John Cheng <johnlicheng@gmail.com>
2021-10-23 22:00:05 -0700

    grs-autocommit:2021-10-23T22:00:05-07:00

--- site-start.el
+++ site-start.el
@@ -147,7 +147,6 @@
   (define-key org-mode-map (kbd "C-c C-n") nil) ;; next heading, but cut across different levels
   (define-key org-mode-map (kbd "C-c C-p") nil) ;; prev heading
   (define-key org-mode-map (kbd "C-c s") 'org-show-subtree)
-  (define-key org-agenda-mode-map (kbd "M-s M-s") 'org-save-all-org-buffers)
   (define-key org-mode-map (kbd "C-c C-x C-f") nil) ;; Unset org-emphasize, conflicts with search
   (define-key org-mode-map (kbd "M-h") 'backward-kill-word) 
   (set-fill-column 120))
